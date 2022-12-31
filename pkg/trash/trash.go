931f225182927bdf5fdf1d9a6df9b73dbed02d73
Sandro Badame <sbadame@gmail.com>
2022-12-31 00:19:11 -0500

    Actually add the pkg code...

---
+++ pkg/trash/trash.go
@@ -0,0 +1,205 @@
+package trash
+
+import (
+	"fmt"
+	"time"
+)
+
+// Pickup is from the types defined in: https://www.yonkersny.gov/home/showpublisheddocument/30455/637743931515170000
+type Pickup int
+const (
+	UNDEFINED         Pickup = iota
+	NO_PICKUP         Pickup = iota
+	NO_PICKUP_HOLIDAY Pickup = iota
+	TRASH             Pickup = iota
+	PAPER             Pickup = iota
+	COMMINGLES        Pickup = iota
+)
+
+func FromString(s string) Pickup {
+	return map[string]Pickup{
+		"cardboard":  PAPER,
+		"commingles": COMMINGLES,
+		"trash":      TRASH,
+	}[s]
+}
+
+func (p Pickup) String() string {
+	switch p {
+	case UNDEFINED:
+		return "<Undefined>"
+	case NO_PICKUP:
+		return "No Pickup"
+	case NO_PICKUP_HOLIDAY:
+		return "No Pickup because of a holiday"
+	case TRASH:
+		return "Trash"
+	case PAPER:
+		return "Paper"
+	case COMMINGLES:
+		return "Commingles"
+	}
+	return "unknown"
+}
+
+type simpleTime struct {
+	year  int
+	month time.Month
+	day   int
+}
+
+var special = map[simpleTime]Pickup{
+	{2022, time.May, 25}:       PAPER,
+	{2022, time.May, 30}:       NO_PICKUP_HOLIDAY,
+	{2022, time.May, 31}:       TRASH,
+	{2022, time.June, 1}:       TRASH,
+	{2022, time.June, 2}:       COMMINGLES,
+	{2022, time.June, 3}:       TRASH,
+	{2022, time.June, 8}:       PAPER,
+	{2022, time.June, 15}:      COMMINGLES,
+	{2022, time.June, 22}:      PAPER,
+	{2022, time.June, 29}:      COMMINGLES,
+	{2022, time.July, 4}:       NO_PICKUP_HOLIDAY,
+	{2022, time.July, 5}:       TRASH,
+	{2022, time.July, 6}:       NO_PICKUP_HOLIDAY,
+	{2022, time.July, 7}:       PAPER,
+	{2022, time.July, 8}:       NO_PICKUP_HOLIDAY,
+	{2022, time.July, 13}:      COMMINGLES,
+	{2022, time.July, 20}:      PAPER,
+	{2022, time.July, 27}:      COMMINGLES,
+	{2022, time.August, 3}:     PAPER,
+	{2022, time.August, 10}:    COMMINGLES,
+	{2022, time.August, 17}:    PAPER,
+	{2022, time.August, 24}:    COMMINGLES,
+	{2022, time.August, 31}:    PAPER,
+	{2022, time.September, 5}:  NO_PICKUP_HOLIDAY,
+	{2022, time.September, 6}:  TRASH,
+	{2022, time.September, 7}:  NO_PICKUP_HOLIDAY,
+	{2022, time.September, 8}:  COMMINGLES,
+	{2022, time.September, 9}:  TRASH,
+	{2022, time.September, 14}: PAPER,
+	{2022, time.September, 21}: COMMINGLES,
+	{2022, time.September, 28}: PAPER,
+	{2022, time.October, 5}:    COMMINGLES,
+	{2022, time.October, 10}:   NO_PICKUP_HOLIDAY,
+	{2022, time.October, 11}:   TRASH,
+	{2022, time.October, 12}:   NO_PICKUP_HOLIDAY,
+	{2022, time.October, 13}:   PAPER,
+	{2022, time.October, 14}:   TRASH,
+	{2022, time.October, 19}:   COMMINGLES,
+	{2022, time.October, 26}:   PAPER,
+	{2022, time.November, 2}:   COMMINGLES,
+	{2022, time.November, 3}:   TRASH,
+	{2022, time.November, 9}:   PAPER,
+	{2022, time.November, 11}:  NO_PICKUP_HOLIDAY,
+	{2022, time.November, 16}:  COMMINGLES,
+	{2022, time.November, 23}:  PAPER,
+	{2022, time.November, 24}:  NO_PICKUP_HOLIDAY,
+	{2022, time.November, 25}:  TRASH,
+	{2022, time.November, 30}:  COMMINGLES,
+	{2022, time.December, 7}:   PAPER,
+	{2022, time.December, 14}:  COMMINGLES,
+	{2022, time.December, 21}:  PAPER,
+	{2022, time.December, 26}:  NO_PICKUP_HOLIDAY,
+	{2022, time.December, 27}:  TRASH,
+	{2022, time.December, 28}:  NO_PICKUP_HOLIDAY,
+	{2022, time.December, 29}:  COMMINGLES,
+	{2022, time.December, 30}:  TRASH,
+	{2023, time.January, 1}: NO_PICKUP_HOLIDAY,
+	{2023, time.January, 5}: PAPER,
+	{2023, time.January, 12}: COMMINGLES,
+	{2023, time.January, 17}: NO_PICKUP_HOLIDAY,
+	{2023, time.January, 18}: TRASH,
+	{2023, time.January, 20}: PAPER,
+	{2023, time.January, 21}: TRASH,
+	{2023, time.January, 26}: COMMINGLES,
+	{2023, time.February, 2}: PAPER,
+	{2023, time.February, 9}: COMMINGLES,
+	{2023, time.February, 16}: PAPER,
+	{2023, time.February, 21}: NO_PICKUP_HOLIDAY,
+	{2023, time.February, 22}: TRASH,
+	{2023, time.February, 23}: NO_PICKUP_HOLIDAY,
+	{2023, time.February, 24}: COMMINGLES,
+	{2023, time.February, 25}: TRASH,
+	{2023, time.March, 2}: PAPER,
+	{2023, time.March, 9}: COMMINGLES,
+	{2023, time.March, 16}: PAPER,
+	{2023, time.March, 23}: COMMINGLES,
+	{2023, time.March, 30}: PAPER,
+	{2023, time.April, 6}: COMMINGLES,
+	{2023, time.April, 13}: PAPER,
+	{2023, time.April, 20}: COMMINGLES,
+	{2023, time.April, 27}: PAPER,
+	{2023, time.May, 4}: COMMINGLES,
+	{2023, time.May, 11}: PAPER,
+	{2023, time.May, 18}: COMMINGLES,
+	{2023, time.May, 25}: PAPER,
+	{2023, time.May, 30}: NO_PICKUP_HOLIDAY,
+	{2023, time.May, 31}: TRASH,
+	{2023, time.June, 1}: NO_PICKUP_HOLIDAY,
+	{2023, time.June, 2}: COMMINGLES,
+	{2023, time.June, 3}: TRASH,
+	{2023, time.July, 4}: NO_PICKUP_HOLIDAY,
+	{2023, time.July, 5}: TRASH,
+	{2023, time.July, 6}: NO_PICKUP_HOLIDAY,
+	{2023, time.July, 7}: PAPER,
+	{2023, time.July, 8}: TRASH,
+	{2023, time.July, 13}: COMMINGLES,
+	{2023, time.July, 20}: PAPER,
+	{2023, time.July, 27}: COMMINGLES,
+	{2023, time.August, 3}: PAPER,
+	{2023, time.August, 10}: COMMINGLES,
+	{2023, time.August, 17}: PAPER,
+	{2023, time.August, 24}: COMMINGLES,
+	{2023, time.September, 5}: NO_PICKUP_HOLIDAY,
+	{2023, time.September, 6}: TRASH,
+	{2023, time.September, 7}: NO_PICKUP_HOLIDAY,
+	{2023, time.September, 8}: COMMINGLES,
+	{2023, time.September, 9}: TRASH,
+	{2023, time.September, 14}: PAPER,
+	{2023, time.September, 21}: COMMINGLES,
+	{2023, time.September, 28}: PAPER,
+	{2023, time.October, 5}: COMMINGLES,
+	{2023, time.October, 10}: NO_PICKUP_HOLIDAY,
+	{2023, time.October, 11}: TRASH,
+	{2023, time.October, 12}: NO_PICKUP_HOLIDAY,
+	{2023, time.October, 13}: PAPER,
+	{2023, time.October, 14}: TRASH,
+	{2023, time.November, 2}: COMMINGLES,
+	{2023, time.November, 9}: PAPER,
+	{2023, time.November, 11}: NO_PICKUP_HOLIDAY,
+	{2023, time.November, 16}: COMMINGLES,
+	{2023, time.November, 23}: PAPER,
+	{2023, time.November, 24}: NO_PICKUP_HOLIDAY,
+	{2023, time.November, 25}: TRASH,
+	{2023, time.November, 30}: COMMINGLES,
+	{2023, time.December, 7}: PAPER,
+	{2023, time.December, 14}: COMMINGLES,
+	{2023, time.December, 21}: PAPER,
+	{2023, time.December, 26}: NO_PICKUP_HOLIDAY,
+	{2023, time.December, 27}: TRASH,
+	{2023, time.December, 28}: NO_PICKUP_HOLIDAY,
+	{2023, time.December, 29}: COMMINGLES,
+	{2023, time.December, 30}: TRASH,
+}
+
+func ForDate(when time.Time) Pickup {
+	p, found := special[simpleTime{year: when.Year(), month: when.Month(), day: when.Day()}]
+	if found {
+		return p
+	}
+	if when.Weekday() == time.Monday || when.Weekday() == time.Thursday {
+		return TRASH
+	}
+	return NO_PICKUP
+}
+
+func NextPickup(starting time.Time, pickup Pickup) (time.Time, error) {
+	for i := 0; i < 14; i++ {
+		starting = starting.Add(time.Hour * 24)
+		if ForDate(starting) == pickup {
+			return starting, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("No upcoming %s pickup.", pickup)
+}
---
+++ pkg/trash/trash_test.go
@@ -0,0 +1,58 @@
+package trash
+
+import (
+	"testing"
+	"time"
+)
+
+func TestForDate(t *testing.T) {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		t.Fatal("Unable to load 'America/New_York' as a location.")
+	}
+	tests := []struct {
+		time time.Time
+		want Pickup
+	}{
+		{time: time.Date(2022, time.May, 25, 0, 0, 0, 0, loc), want: PAPER},
+		{time: time.Date(2022, time.May, 30, 0, 0, 0, 0, loc), want: NO_PICKUP_HOLIDAY},
+		{time: time.Date(2022, time.June, 3, 0, 0, 0, 0, loc), want: TRASH},
+		{time: time.Date(2022, time.June, 13, 0, 0, 0, 0, loc), want: TRASH},
+		{time: time.Date(2022, time.June, 15, 0, 0, 0, 0, loc), want: COMMINGLES},
+		{time: time.Date(2022, time.June, 15, 1, 1, 1, 1, loc), want: COMMINGLES},
+	}
+
+	for _, tc := range tests {
+		got := ForDate(tc.time)
+		if got != tc.want {
+			t.Errorf("date: %v got: %v want: %v", tc.time, got, tc.want)
+		}
+	}
+}
+
+func TestNextPickup(t *testing.T) {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		t.Fatal("Unable to load 'America/New_York' as a location.")
+	}
+	tests := []struct {
+		from    time.Time
+		pickup  Pickup
+		want    time.Time
+		wantErr error
+	}{
+		{
+			from:    time.Date(2022, time.May, 27, 0, 0, 0, 0, loc),
+			pickup:  TRASH,
+			want:    time.Date(2022, time.May, 31, 0, 0, 0, 0, loc),
+			wantErr: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := NextPickup(tc.from, tc.pickup)
+		if err != tc.wantErr || got != tc.want {
+			t.Errorf("from: %v pickup: %v, got: %v err: %v, want: %v, wantErr: %v", tc.from, tc.pickup, got, err, tc.want, tc.wantErr)
+		}
+	}
+}
